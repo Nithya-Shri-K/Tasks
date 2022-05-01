3697a6bdfaa5d195249d2d1a1cad29302eee7a0e
Nithya-Shri-K <93898246+Nithya-Shri-K@users.noreply.github.com>
2022-05-02 00:23:55 +0530

    Add files via upload

---
+++ Week 7 - Equality/Kotlin.kt
@@ -0,0 +1,24 @@
+import java.util.*
+
+// Compiler automatically creates the equals, hashCode, toString, and copy functions
+data class Book(val bookId: Int, val bookName: String, val totalQuantity: Int)
+
+fun main(){
+
+    val book1 = Book(1,"You",5)
+    val book2 = Book(1,"You",5)
+    val book3 = Book(2,"The secret",6)
+
+    // ==( Structural equality ) internally calls equals method
+    println(book1 == book2) // true
+
+    // referential equality
+    println(book1 === book2) // false
+    println(book1.hashCode()) // 2792454
+    println(book2.hashCode()) // 2792454
+    println(book3.hashCode()) // 158664296
+
+    // checking equality with hashcode is faster than equals
+    println(book1.hashCode() == book2.hashCode())
+ }
+
